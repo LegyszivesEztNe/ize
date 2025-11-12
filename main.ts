a4722bb2468c7a6ffb8e0c21b0babeb2f7ed3fd7
LegyszivesEztNe <legysziveseztne@gmail.com>
2025-11-12 08:31:13 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 5))
+    basic.pause(2000)
+    basic.clearScreen()
+})
+basic.clearScreen()
+basic.forever(function () {
+	
+})
