87a69c8da9692b0fca10e9c1498e39bfa3012bb9
LegyszivesEztNe <legysziveseztne@gmail.com>
2025-11-12 08:41:50 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Mocskos Fidesz!")
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 5))
     basic.pause(2000)
     basic.clearScreen()
 })
+basic.showIcon(IconNames.Heart)
+basic.pause(100)
 basic.clearScreen()
 basic.forever(function () {
 	
